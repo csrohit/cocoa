fd77e1e782fe9c692225c2875d25378a7e4b2880
Rohit Nimkar <nehalnimkar@gmail.com>
2023-11-11 17:07:54 +0530

    Separate into multiple files

---
+++ AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  BlankWindow
+//
+//  Created by Rohit Nimkar on 11/11/23.
+//
+
+#ifndef AppDelegate_h
+#define AppDelegate_h
+
+#import <Cocoa/Cocoa.h>
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (strong, nonatomic) NSWindow *window;
+
+@end
+
+#endif /* AppDelegate_h */
--- BlankWindow/main.m
+++ BlankWindow/main.m
@@ -5,12 +5,25 @@
 //  Created by Rohit Nimkar on 11/11/23.
 //
 
-#import <Foundation/Foundation.h>
+#import <Cocoa/Cocoa.h>
+#import "AppDelegate.h"
 
+
+/* Program entry point */
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        /* Handle of the application, handles main loop and windows/menus
+         https://developer.apple.com/documentation/appkit/nsapplication
+         shared class function initializes the display environment and connects to
+         the window server & display server
+         */
+        NSApplication *application = [NSApplication sharedApplication];
+        AppDelegate *appDelegate = [[AppDelegate alloc] init];
+        [application setDelegate:appDelegate];
+        
+        /* Start the main application loop */
+        [application run];
     }
     return 0;
 }
