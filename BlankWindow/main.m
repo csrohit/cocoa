fddd40fc2a7242b808f8ecb671015e2fffcc0153
Rohit Nimkar <nehalnimkar@gmail.com>
2023-11-11 09:34:45 +0530

    Initial Commit

---
+++ BlankWindow/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BlankWindow
+//
+//  Created by Rohit Nimkar on 11/11/23.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
